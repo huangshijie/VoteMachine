682fc2f6ce7d18114051e04f0a9dfe282c013fab
huang shijie <alex.huang02@sap.com>
2019-05-27 14:37:35 +0800

    init rep

---
+++ src/main/java/org/huang/vote/App.java
@@ -0,0 +1,13 @@
+package org.huang.vote;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/org/huang/vote/service/BaseService.java
@@ -0,0 +1,5 @@
+package org.huang.vote.service;
+
+public interface BaseService {
+
+}
